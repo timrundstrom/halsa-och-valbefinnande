e7430cfbae017a494289baaba9ec60411451e0de
StefaK <StefAzaz@outlook.com>
2019-01-25 11:15:23 +0100

    Live server and file organization

---
+++ public/data/scripts/script.js
@@ -0,0 +1,26 @@
+$(() => {
+    $(document).on("click", "#pg1", function() {
+        $("#pg1").addClass("hidden");
+        $("#pg2").removeClass("hidden");
+    })
+
+    $(document).on("click", "#pg2", function() {
+        $("#pg2").addClass("hidden");
+        $("#pg3").removeClass("hidden");
+    })
+
+    $(document).on("click", "#pg3", function() {
+        $("#pg3").addClass("hidden");
+        $("#pg4").removeClass("hidden");
+    })
+
+    $(document).on("click", "#pg4", function() {
+        $("#pg4").addClass("hidden");
+        $("#pg1").removeClass("hidden");
+    })
+
+    $("li label").hover(_ => {
+        $("#pg1").toggleClass("blur");
+        $("#background").toggleClass("blur");
+    });
+});
