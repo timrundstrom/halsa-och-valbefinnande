0f94332946040495fd9ee1859b6aee3f58df5c4f
Tim Rundström <timmi.rundstrom@gmail.com>
2019-02-09 02:29:00 +0100

    Funktioner till sidorna
    * Gjorde nti loggan och nav knapparna mer synliga med en svart stroke.
    * Bytte ut nti bilden med en med hd.
    * Gav index.html en slideshow funktion
    * Gjorde en liten parallax till stora bilderna.
    * Bytte highlight färgen på nav knapparna.
    * Gjorde att pilarna tar dig ner till rubriken (smooth)
    * Loggan är en länk till skolan.
    * alla nav knappar förutom "program" fungerar. (vad ska "program" gå till?)
    * Bytte font på rubrikerna.
    Tror det var allt.
    @diablobounds
    
    Tror allt med det som finns är klart. Är bara att lägga till all info och styla det.

--- alternativ sida/scripts/script.js
+++ alternativ sida/scripts/script.js
@@ -1,3 +1,5 @@
+const img = document.getElementById("pic");
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if ($(this).scrollTop())
@@ -11,21 +13,36 @@ $(document).ready(function(){
             $(".box").css({"opacity" : "1"})
             $('nav').removeClass('black');
         }
+        parallax()
     })
 
-    
     // Ifall det är index.html som är laddat ska bilderna bytas efter varje intervall.
-    const filename = window.location.pathname.substring(url.lastIndexOf('/')+1);
-    if (filename == "index.html"){
+    const url = window.location.pathname;
+    const filename = url.substring(url.lastIndexOf('/')+1);
+    if (filename === "index.html"){
+        // Bilder att slideshowa och delay mellan (OBS! transition är 1s)
         const images = ["avenue", "backpack", "daisy", "grassfield", "hjalstaviken", "jump", "rooftop"];
+        const slidetime = 5000;
         let index = images.length;
+        img.style.transition = "all 1s ease";
         setInterval(function(){
-            if (index === 7){index = 0;}
-            document.getElementById("pic").style.backgroundImage = `url(../images/${images[index]}.jpg)`;
+            if (index === images.length){index = 0;}
+            img.style.backgroundImage = `url(../images/${images[index]}.jpg)`;
             index++;
-        }, 60000)
+        }, slidetime)
     }
-    // Annars sätt NTI bild (eftersom det är garanterat om-oss)
-    else{document.getElementById("pic").style.backgroundImage = "url(../images/NTI.jpg)"}
-
+    /* Annars sätt NTI bild (eftersom det är garanterat om-oss)
+    Om fler sidor ska läggas till så ändra lägg till en else if.*/
+    else{img.style.backgroundImage = "url(../images/NTI.png)";}
 })
+
+function parallax(){
+    img.classList.add('notransition');
+
+    const st = $(this).scrollTop();
+    const newValue = 150 - st / 20;
+    img.style.backgroundSize = `${newValue}%`;
+
+    img.offsetHeight;
+    img.classList.remove('notransition')
+}
